df3676abcd1f8601234a57e152082e208b43c155
Eclypse2D <46421694+Eclypse2D@users.noreply.github.com>
2019-01-30 17:33:50 +0200

    Create filename.py

---
+++ filename.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+bot = commands.Bot(command_prefix='Slave')
+
+@bot.event
+async def on_ready():
+	print('The Bot is ready!')
+	print(bot.user.name)
+	print(bot.user.id)
+
+@bot.command()
+async def ping():
+	await bot.say('ping pong')
+	await bot.say('Why did you ping me tho?')
+	
+@bot.command()
+async def thing():	
+	await bot.say('@Sl𝓐ve Thing#0468 is bad')
+
+@bot.command(pass_context=True) 
+async def mute(ctx,target:discord.Member):
+	role=discord.utils.get(ctx.message.server.roles,name='Muted')
+	
+	await bot.add_roles(target,role)
+	
+@bot.command(pass_context=True) 
+async def warn(ctx,target:discord.Member):
+	await bot.send_message(target,'Warning!')
+	
+@bot.command(pass_context=True)
+async def kick(ctx,target:discord.Member):
+	await bot.kick(target)
+	
+@bot.command(pass_context=True)
+async def ban(ctx,target:discord.Member):
+	await bot.ban(target)
+
+bot.run('NTMxMTY0MDUzMjg5MzA0MDc0.DxJ9VA.rLYqQzO-xsc051ZDp5aQtYaNmlU')
