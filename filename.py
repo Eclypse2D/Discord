8d1192e65fd9bd012fd3c4347ffab7f417bcf95a
Eclypse2D <46421694+Eclypse2D@users.noreply.github.com>
2019-04-04 08:44:55 +0300

    Update filename.py

--- filename.py
+++ filename.py
@@ -604,7 +604,7 @@ async def ping(ctx):
 
 @bot.command(pass_context = True)
 @commands.has_permissions(administrator=True) 
-async def announce(ctx, channel: discord.Channel=None, *, msg: str):
+
     r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
     embed=discord.Embed(title="Announcement", description="{}".format(msg), color = discord.Color((r << 16) + (g << 8) + b))
     await bot.send_message(channel, embed=embed)
