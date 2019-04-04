b8a5a56579ac505ef7adbdfab658419941789102
Eclypse2D <46421694+Eclypse2D@users.noreply.github.com>
2019-04-04 08:42:09 +0300

    Update filename.py

--- filename.py
+++ filename.py
@@ -10,9 +10,7 @@ import os
 import functools
 import time
 import datetime
-
 import json
-import praw
 import aiohttp
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
